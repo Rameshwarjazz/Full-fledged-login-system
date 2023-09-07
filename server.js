b41c1e83506c39b32bf3943cc7f042daa4ba6238
Rameshwar <rjsahab143@gmail.com>
2023-09-07 10:11:57 +0530

    login

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/login-system'
+, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post(
+  '/register',
+  [
+    body('username').isLength({ min: 1 }).trim().withMessage('Username is required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already in use. Please choose another one.' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const user = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    res.status(200).json({ message: 'Registration successful! Please log in.' });
+  }
+);
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  // Find user by username
+  const user = await User.findOne({ username });
+
+  if (user) {
+    // Check the password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      req.session.userId = user._id;
+      return res.status(200).json({ message: 'Login successful!' });
+    }
+  }
+
+  res.status(401).json({ message: 'Invalid username or password' });
+});
+
+app.post('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.status(200).json({ message: 'Logout successful!' });
+  });
+});
+
+app.get('/dashboard', (req, res) => {
+  if (req.session.userId) {
+    res.status(200).json({ message: 'Welcome to the dashboard!' });
+  } else {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
